a2e56641f94a3c08385e5c986de11c10b28f5960
VirgiawanLR <116149015+VirgiawanLR@users.noreply.github.com>
2023-04-18 11:02:59 +0700

    initialize sign-up page

---
+++ src/components/user/registration/TermsAndCondition.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+
+function TermsAndCondition() {
+  return (
+    <div>
+      <h1>Terms and Conditions</h1>
+
+      <h2>Introduction</h2>
+      <p>
+        These Website Standard Terms and Conditions written on this webpage
+        shall manage your use of our website, Webiste Name accessible at
+        www.example.com.
+      </p>
+
+      <h2>Intellectual Property Rights</h2>
+      <p>
+        Unless otherwise stated, we or our licensors own the intellectual
+        property rights in the website and material on the website. Subject to
+        the license below, all these intellectual property rights are reserved.
+      </p>
+
+      <h2>Restrictions</h2>
+      <p>You are specifically restricted from all of the following:</p>
+      <ul>
+        <li>publishing any website material in any other media;</li>
+        <li>
+          selling, sublicensing and/or otherwise commercializing any website
+          material;
+        </li>
+        <li>publicly performing and/or showing any website material;</li>
+        <li>
+          using this website in any way that is or may be damaging to this
+          website;
+        </li>
+        <li>
+          using this website in any way that impacts user access to this
+          website;
+        </li>
+        <li>
+          using this website contrary to applicable laws and regulations, or in
+          a way that causes, or may cause, harm to the website, or to any person
+          or business entity;
+        </li>
+        <li>
+          engaging in any data mining, data harvesting, data extracting or any
+          other similar activity in relation to this website, or while using
+          this website;
+        </li>
+        <li>using this website to engage in any advertising or marketing;</li>
+      </ul>
+
+      <h2>Indemnification</h2>
+      <p>
+        You agree to indemnify, defend and hold us and our affiliates, partners,
+        officers, directors, agents, contractors, licensors, service providers,
+        subcontractors, suppliers, interns and employees, harmless from any
+        claim or demand, including reasonable attorneys' fees, made by any
+        third-party due to or arising out of your breach of these Terms of
+        Service or the documents they incorporate by reference, or your
+        violation of any law or the rights of a third-party.
+      </p>
+
+      <h2>Termination</h2>
+      <p>
+        We may terminate or suspend access to our Service immediately, without
+        prior notice or liability, for any reason whatsoever, including without
+        limitation if you breach the Terms.
+      </p>
+
+      <h2>Governing Law</h2>
+      <p>
+        These Terms shall be governed and construed in accordance with the laws
+        of the State of Indonesia, United States, without regard to its conflict
+        of law provisions.
+      </p>
+
+      <h2>Contact Us</h2>
+      <p>
+        If you have any questions about these Terms and Conditions, please
+        contact us at admin@tokoku.mail.com
+      </p>
+    </div>
+  );
+}
+
+export default TermsAndCondition;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        tertiary: "#16a34a",
+        dark: "#334155",
+        secondary: "#64748b",
+      },
+    },
   },
   plugins: [],
 };

1bfece735a99258f7a79aac459a25c9497edbfa7
VirgiawanLR <116149015+VirgiawanLR@users.noreply.github.com>
2023-04-18 16:08:59 +0700

    Update tailwind.config.js
    add color template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2EC4B6",
+        tertiary: "#FFBF69",
+        light: "#CBF3F0",
+        secondary: "#FF9F1C",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/images/cart-icon.png')",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
 };

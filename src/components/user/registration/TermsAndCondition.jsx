38979f3c84cf556d8ce8e7f5d737d1cc0b0b4bb5
VirgiawanLR <116149015+VirgiawanLR@users.noreply.github.com>
2023-04-18 22:30:11 +0700

    Update TermsAndCondition.jsx

--- src/components/user/registration/TermsAndCondition.jsx
+++ src/components/user/registration/TermsAndCondition.jsx
@@ -3,54 +3,65 @@ import React from "react";
 function TermsAndCondition() {
   return (
     <div>
-      <h1>Terms and Conditions</h1>
+      <h1 className=" text-6xl text-primary pt-8 pb-16 font-black leading-snug">
+        Terms and Conditions
+      </h1>
 
-      <h2>Introduction</h2>
-      <p>
+      <h2 className=" text-2xl text-secondary font-semibold py-6">
+        Introduction
+      </h2>
+      <p className=" text-gray-800 pb-10">
         These Website Standard Terms and Conditions written on this webpage
         shall manage your use of our website, Webiste Name accessible at
         www.example.com.
       </p>
 
-      <h2>Intellectual Property Rights</h2>
-      <p>
+      <h2 className=" text-2xl text-secondary font-semibold py-6">
+        Intellectual Property Rights
+      </h2>
+      <p className=" text-gray-800 pb-10">
         Unless otherwise stated, we or our licensors own the intellectual
         property rights in the website and material on the website. Subject to
         the license below, all these intellectual property rights are reserved.
       </p>
 
-      <h2>Restrictions</h2>
-      <p>You are specifically restricted from all of the following:</p>
-      <ul>
-        <li>publishing any website material in any other media;</li>
+      <h2 className=" text-2xl text-secondary font-semibold py-6">
+        Restrictions
+      </h2>
+      <p className=" text-gray-800 pb-4">
+        You are specifically restricted from all of the following:
+      </p>
+      <ul className=" text-gray-800 pb-10 space-y-2 list-disc">
+        <li>publishing any website material in any other media</li>
         <li>
           selling, sublicensing and/or otherwise commercializing any website
-          material;
+          material
         </li>
-        <li>publicly performing and/or showing any website material;</li>
+        <li>publicly performing and/or showing any website material</li>
         <li>
           using this website in any way that is or may be damaging to this
-          website;
+          website
         </li>
         <li>
-          using this website in any way that impacts user access to this
-          website;
+          using this website in any way that impacts user access to this website
         </li>
         <li>
           using this website contrary to applicable laws and regulations, or in
           a way that causes, or may cause, harm to the website, or to any person
-          or business entity;
+          or business entity
         </li>
         <li>
           engaging in any data mining, data harvesting, data extracting or any
           other similar activity in relation to this website, or while using
-          this website;
+          this website
         </li>
-        <li>using this website to engage in any advertising or marketing;</li>
+        <li>using this website to engage in any advertising or marketing</li>
       </ul>
 
-      <h2>Indemnification</h2>
-      <p>
+      <h2 className=" text-2xl text-secondary font-semibold py-6">
+        Indemnification
+      </h2>
+      <p className=" text-gray-800 pb-10">
         You agree to indemnify, defend and hold us and our affiliates, partners,
         officers, directors, agents, contractors, licensors, service providers,
         subcontractors, suppliers, interns and employees, harmless from any
@@ -60,22 +71,28 @@ function TermsAndCondition() {
         violation of any law or the rights of a third-party.
       </p>
 
-      <h2>Termination</h2>
-      <p>
+      <h2 className=" text-2xl text-secondary font-semibold py-6">
+        Termination
+      </h2>
+      <p className=" text-gray-800 pb-10">
         We may terminate or suspend access to our Service immediately, without
         prior notice or liability, for any reason whatsoever, including without
         limitation if you breach the Terms.
       </p>
 
-      <h2>Governing Law</h2>
-      <p>
+      <h2 className=" text-2xl text-secondary font-semibold py-6">
+        Governing Law
+      </h2>
+      <p className=" text-gray-800 pb-10">
         These Terms shall be governed and construed in accordance with the laws
-        of the State of Indonesia, United States, without regard to its conflict
-        of law provisions.
+        of the State of Indonesia without regard to its conflict of law
+        provisions.
       </p>
 
-      <h2>Contact Us</h2>
-      <p>
+      <h2 className=" text-2xl text-secondary font-semibold py-6">
+        Contact Us
+      </h2>
+      <p className=" text-gray-800 pb-10">
         If you have any questions about these Terms and Conditions, please
         contact us at admin@tokoku.mail.com
       </p>

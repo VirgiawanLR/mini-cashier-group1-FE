e270d0485520bfeb7b404294f5149d610a647e6f
Priska Rafel <60578867+moonrafel@users.noreply.github.com>
2023-04-24 13:07:01 +0700

    Coding lebih rapi

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,5 +1,3 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom"
 import { nextPage, prevPage } from "../features/products/productSlice";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -9,50 +7,9 @@ function Pagination() {
   const selectProductList = useSelector((state) => state.product.selectProductList); 
   const pageData = useSelector((state) => state.product.pageData); 
 
-  // const totalPages = Math.ceil(totalItems / itemsPerPage);
-
-  // useEffect
-
-  // useEffect(() => {
-  //   setOffset(itemsPerPage * currentPage);
-  // }, [currentPage]);
-
-  // useEffect(() => {
-  //   fetchPokemons();
-  // }, [offset]);
-
-  // { totalItems, onPageChange, itemsPerPageOptions = [10, 25, 50] }
-
-  // const handlePageChange = (pageNumber) => {
-  //   setCurrentPage(pageNumber);
-  //   onPageChange(pageNumber);
-  // };
-
-  // const handleItemsPerPageChange = (event) => {
-  //   const newItemsPerPage = parseInt(event.target.value);
-  //   setItemsPerPage(newItemsPerPage);
-  //   handlePageChange(1);
-  // };
-
-  // const paginationItems = [];
-  // for (let i = 1; i <= totalPages; i++) {
-  //   paginationItems.push(
-  //     <li key={i} className={`mx-1 ${currentPage === i ? 'text-blue-700 font-bold' : 'text-gray-500'}`}>
-  //       <button className="px-2 py-1 rounded-lg hover:text-blue-700 focus:outline-none" onClick={() => handlePageChange(i)}>
-  //         {i}
-  //       </button>
-  //     </li>
-  //   );
-  // }
-
-  // const itemsPerPageOptionsElements = itemsPerPageOptions.map((option) => (
-  //   <option key={option} value={option}>
-  //     {option}
-  //   </option>
-  // ));
-
   return (
     <div>
+
       <div className="flex flex-row justify-center items-center gap-2 p-2 h-full w-full bg-amber-400">
         <button
           className={`px-2 py-1 rounded-lg focus:outline-none ${
@@ -80,43 +37,8 @@ function Pagination() {
           NEXT
         </button>
       </div>
+      
     </div>
-    // <nav className="flex flex-col items-center my-4">
-    //   <ul className="flex">
-    //     <li className="mr-2">
-    //       <button
-    //         className={`px-2 py-1 rounded-lg focus:outline-none ${currentPage === 1 ? 'bg-gray-200 text-gray-500 cursor-not-allowed' : 'bg-white text-gray-600 hover:text-blue-700'}`}
-    //         disabled={currentPage === 1}
-    //         onClick={() => handlePageChange(currentPage - 1)}
-    //       >
-    //         Previous
-    //       </button>
-    //     </li>
-    //     {paginationItems}
-    //     <li className="ml-2">
-    //       <button
-    //         className={`px-2 py-1 rounded-lg focus:outline-none ${currentPage === totalPages ? 'bg-gray-200 text-gray-500 cursor-not-allowed' : 'bg-white text-gray-600 hover:text-blue-700'}`}
-    //         disabled={currentPage === totalPages}
-    //         onClick={() => handlePageChange(currentPage + 1)}
-    //       >
-    //         Next
-    //       </button>
-    //     </li>
-    //   </ul>
-    //   <div className="mt-4">
-    //     <label htmlFor="itemsPerPageSelect" className="text-gray-600 mr-2">
-    //       Items per page:
-    //     </label>
-    //     <select
-    //       id="itemsPerPageSelect"
-    //       className="px-2 py-1 rounded-lg border border-gray-400 focus:outline-none"
-    //       value={itemsPerPage}
-    //       onChange={handleItemsPerPageChange}
-    //     >
-    //       {itemsPerPageOptionsElements}
-    //     </select>
-    //   </div>
-    // </nav>
   );
 }
 
--- src/components/products/ProductHome.jsx
+++ src/components/products/ProductHome.jsx
@@ -1,56 +1,59 @@
-import { useEffect } from "react"
-import { useDispatch, useSelector } from 'react-redux'
-import ProdHomeCard from "./ProdHomeCard"
-import { getProducts, fetchProducts } from "../../features/products/productSlice"
-import Pagination from "../Pagination"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ProdHomeCard from "./ProdHomeCard";
+import {
+  getProducts,
+  fetchProducts,
+} from "../../features/products/productSlice";
+import Pagination from "../Pagination";
 
 function ProductHome() {
+  const dispatch = useDispatch();
+  const selectProductList = useSelector(
+    (state) => state.product.selectProductList
+  );
 
-    const dispatch = useDispatch()
-    const selectProductList = useSelector((state) => state.product.selectProductList)
+  const renderProductList = () => {
+    return selectProductList.map((product) => {
+      return <ProdHomeCard product={product} />;
+    });
+  };
 
-    const renderProductList = () => {
-        return selectProductList.map((product) => {
-            return <ProdHomeCard product={product} />
-        })
-    }
+  useEffect(() => {
+    dispatch(getProducts());
+  }, []);
 
-    useEffect(() => {
-        dispatch(getProducts())
-    }, [])
+  useEffect(() => {
+    renderProductList();
+    dispatch(fetchProducts());
+  }, [selectProductList]);
 
-    useEffect(() => {
-        renderProductList();
-        dispatch(fetchProducts())
-    }, [selectProductList])
+  return (
+    <div className=" h-screen flex flex-col gap-2 mx-auto bg-light p-10">
+      <div>
+        <h1 className="text-2xl font-bold bg-red">Product Home</h1>
+      </div>
 
-
-    return (
-        <div className=" h-screen flex flex-col gap-2 mx-auto bg-light p-10">
-
-            <div>
-            <h1 className="text-2xl font-bold bg-red">Product Home</h1>
-            </div>
-
-{/* cards & pagination */}
-<div className="flex flex-col">
-
-<div className="
+      {/* cards & pagination */}
+      <div className="flex flex-col">
+        
+        <div
+          className="
             grid grid-cols-3 gap-3
             bg-dark
             p-5 h-full w-full
             items-center
             ">
-            {renderProductList()}
-</div>
-<div>
-<Pagination />
-</div>
-
-            </div>
-
+          {renderProductList()}
         </div>
-    )
+        
+        <div>
+          <Pagination />
+        </div>
+      
+      </div>
+    </div>
+  );
 }
 
-export default ProductHome
+export default ProductHome;
